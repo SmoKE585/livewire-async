f62c8c71e91142910b60203f4fc941df75b5dc4b
Alexander Abolmasov <53314888+SmoKE585@users.noreply.github.com>
2022-04-03 18:43:18 +0300

    Update loading.blade.php

--- src/views/loading.blade.php
+++ src/views/loading.blade.php
@@ -1,8 +1,8 @@
 <div wire:init="$set('load', true)">
-        <div class="animate-pulse">
-            <div class="h-2 mb-3 bg-grey-700"></div>
-            <div class="h-2 mb-3 bg-grey-700"></div>
-            <div class="h-2 mb-3 bg-grey-700"></div>
-            <div class="h-2 mb-3 bg-grey-700"></div>
-        </div>
+    <div class="animate__animated animate__fadeIn animate__slow animate__infinite mb-3">
+        <div class="bg-label-secondary mb-2" style="height: 12px"></div>
+        <div class="bg-label-secondary mb-2" style="height: 12px"></div>
+        <div class="bg-label-secondary mb-2" style="height: 12px"></div>
+        <div class="bg-label-secondary mb-2" style="height: 12px"></div>
+    </div>
 </div>
